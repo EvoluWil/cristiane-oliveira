dfd328170e09554c084789d6ac001ce55dcbdeb3
Willian Git <57255222+WrsDeveloper@users.noreply.github.com>
2021-09-14 16:03:23 -0300

    remove midias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,17 +11,13 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         {/* DEFINE LINK DA FONTE POPPINS E FONTE DE ICONES  */}
-        <title>TARGET</title>
+        <title>Cristiane Oliveira</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link rel="icon" href="favicon.svg" sizes="any" type="image/svg+xml" />
+        <link rel="icon" href="logo.png" sizes="any" type="image/svg+xml" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
         />
       </Head>
       {/* THEMEPROVIDER DEIXA O TEMA DA APLICAÇÃO DISPONIVEL EM TODOS ELEMENTOS FILHOS NESSE CASO TODAS AS PAGINAS */}
