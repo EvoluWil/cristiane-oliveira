a4dea023c5cb602fa3f9abde4dfb659e3054f135
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-01-15 18:55:56 -0300

    fix started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+'use client';
 import { HomeAesthetics } from '@/components/partials/home/aesthetics/home-aesthetics';
 import { HomeBeauty } from '@/components/partials/home/beauty/beauty';
 import { HomeHealth } from '@/components/partials/home/health/health';
 import { HomeInitial } from '@/components/partials/home/initial/home-initial';
 import { HomeLocation } from '@/components/partials/home/location/location';
 import { Box } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [started, setStarted] = useState(false);
+  useEffect(() => {
+    setStarted(true);
+  }, []);
+
+  if (!started) {
+    return null;
+  }
+
   return (
     <Box>
       <HomeInitial />
